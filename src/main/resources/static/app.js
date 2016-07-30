721038771f5aee43d70ca27e9afdd453aca6ebe6
d.bakal <d.bakal@controlpay.com>
2016-07-30 21:16:42 +0300

    Added the new angular module 'booking' (the form for Country)

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -2,6 +2,7 @@ var challenge = angular.module('challenge', [
     'challenge.home',
     'challenge.login',
     'challenge.sandbox',
+    'challenge.booking',
     'ui.router',
     'angular-jwt',
     'angular-storage'
---
+++ src/main/resources/static/js/controllers/booking.js
@@ -0,0 +1,39 @@
+angular.module('challenge.booking', [
+    'ui.router'
+]).config(function ($stateProvider) {
+    $stateProvider
+        .state('booking', {
+            url: '/booking',
+            controller: 'booking.controller',
+            templateUrl: 'views/booking/booking.html'
+        })
+        .state('country', {
+            url: '/booking/country',
+            controller: 'country.controller',
+            templateUrl: 'views/booking/addCountry.html'
+        })
+    ;
+
+}).controller('booking.controller', function ($scope, $http) {
+}).controller('country.controller', function ($scope, $http) {
+    $scope.country = "";
+    $scope.sendCountry = function () {
+        $http.post("/country", null, {params: {country: $scope.country}})
+            .success(function(data, status) {
+            })
+            .error(function(data, status) {
+            });
+    };
+    $scope.result = "";
+    $scope.asyncResult = function () {
+        $http.get("/country/result", null)
+            .success(function(data, status) {
+                alert("success");
+                $scope.result="success";
+            })
+            .error(function(data, status) {
+                alert("error");
+            });
+    };
+})
+;
