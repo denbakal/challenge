8b2e15b3420a3044da74465e839cae7031a4232d
Bakal <d.bakal@controlpay.com>
2016-12-27 09:57:57 +0200

    Add NoCacheFilter (solution for handling browser buttons in Spring MVC

---
+++ src/main/java/ua/challenge/core/filter/NoCacheFilter.java
@@ -0,0 +1,33 @@
+package ua.challenge.core.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by d.bakal on 27.12.2016.
+ */
+@WebFilter(urlPatterns = "/*", filterName = "noCacheFilter")
+public class NoCacheFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+        response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+        response.setDateHeader("Expires", 0); // Proxies.
+
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
