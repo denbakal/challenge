a512f61dff286e8251dd9dc700cd94bc3412ae9a
Bakal <d.bakal@controlpay.com>
2017-04-13 11:54:29 +0300

    Hibernate: get a list of managed Entity instances in a persistence context (JPA does not define such a capability)

--- src/test/java/ua/challenge/hibernate/examples/performance/HibernateTestingTest.java
+++ src/test/java/ua/challenge/hibernate/examples/performance/HibernateTestingTest.java
@@ -5,6 +5,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
+import org.hibernate.engine.spi.EntityEntry;
+import org.hibernate.engine.spi.SessionImplementor;
 import org.junit.Test;
 import ua.challenge.hibernate.examples.jpa.JPAUnitTestCase;
 
@@ -12,7 +14,9 @@ import javax.persistence.*;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
 
@@ -60,6 +64,32 @@ public class HibernateTestingTest extends JPAUnitTestCase {
                     .createNativeQuery("select id from persons", Person.class).getResultList();
 
             persons.forEach(System.out::println);
+        });
+    }
+
+    @Test
+    public void getListOfManagedEntityInstancesFromContextTest() {
+        log.debug("Hibernate testing");
+        doInJPA(this::entityManagerFactory, entityManager -> {
+            Phone phone1 = new Phone( "123-456-7890" );
+            Phone phone2 = new Phone( "321-654-0987" );
+
+            Person person = new Person();
+            person.setFirstName("User");
+            person.setLastName("LastUser");
+            person.setPhones(Arrays.asList(phone1, phone2));
+
+            entityManager.persist(person);
+
+            /* Get a list of managed Entity instances in a persistence context */
+            SessionImplementor implementor = entityManager.unwrap(SessionImplementor.class);
+            org.hibernate.engine.spi.PersistenceContext context = implementor.getPersistenceContext();
+            Map.Entry<Object,org.hibernate.engine.spi.EntityEntry>[] entityEntries = context.reentrantSafeEntityEntries();
+            for (Map.Entry<Object, EntityEntry> entityEntry : entityEntries) {
+                log.debug("entityEntry = " + entityEntry.getKey() + " - " + entityEntry.getValue());
+            }
+
+            assertThat(entityEntries.length).isEqualTo(3);
         });
     }
 
