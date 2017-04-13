82174414d40a5f719d70cc4425df40a692fcc652
Bakal <d.bakal@controlpay.com>
2017-04-13 11:41:55 +0300

    Hibernate: sample with hibernate-testing

--- src/test/java/ua/challenge/hibernate/examples/performance/HibernateTestingTest.java
+++ src/test/java/ua/challenge/hibernate/examples/performance/HibernateTestingTest.java
@@ -3,11 +3,15 @@ package ua.challenge.hibernate.examples.performance;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.extern.log4j.Log4j2;
 import org.junit.Test;
 import ua.challenge.hibernate.examples.jpa.JPAUnitTestCase;
 
 import javax.persistence.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
 
@@ -17,21 +21,53 @@ import static org.hibernate.testing.transaction.TransactionUtil.doInJPA;
  */
 @Log4j2
 public class HibernateTestingTest extends JPAUnitTestCase {
+    @Test(expected = ClassCastException.class)
+    public void failedTest() {
+        log.debug("Hibernate testing");
+        doInJPA(this::entityManagerFactory, entityManager -> {
+            Phone phone1 = new Phone( "123-456-7890" );
+            Phone phone2 = new Phone( "321-654-0987" );
+
+            Person person = new Person();
+            person.setFirstName("User");
+            person.setLastName("LastUser");
+            person.setPhones(Arrays.asList(phone1, phone2));
+
+            entityManager.persist(person);
+
+            List<Person> persons = entityManager
+                    .createNativeQuery("select id from persons").getResultList();
+
+            persons.forEach(System.out::println);
+        });
+    }
+
     @Test
     public void test() {
         log.debug("Hibernate testing");
         doInJPA(this::entityManagerFactory, entityManager -> {
-            Phone phone = new Phone();
-            phone.setNumber("123-456-7890");
+            Phone phone1 = new Phone( "123-456-7890" );
+            Phone phone2 = new Phone( "321-654-0987" );
 
-            entityManager.persist(phone);
+            Person person = new Person();
+            person.setFirstName("User");
+            person.setLastName("LastUser");
+            person.setPhones(Arrays.asList(phone1, phone2));
+
+            entityManager.persist(person);
+
+            List<Person> persons = entityManager
+                    .createNativeQuery("select id from persons", Person.class).getResultList();
+
+            persons.forEach(System.out::println);
         });
     }
 
     @Override
     protected Class<?>[] entities() {
         return new Class<?>[] {
-                Phone.class
+                Phone.class,
+                Person.class
         };
     }
 
@@ -39,11 +75,44 @@ public class HibernateTestingTest extends JPAUnitTestCase {
     @Table(name = "phones")
     @Getter
     @Setter
-    @NoArgsConstructor
+    @ToString
     private static class Phone {
         @Id
         @GeneratedValue
         private Long id;
         private String number;
+
+        public Phone() {
+        }
+
+        public Phone(String number) {
+            this.number = number;
+        }
+    }
+
+    @Entity
+    @Table(name = "persons")
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    private static class Person {
+        @Id
+        @GeneratedValue
+        private Long id;
+
+        @OneToMany(cascade = CascadeType.ALL)
+        private List<Phone> phones;
+
+        private String firstName;
+        private String LastName;
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "id=" + id +
+                    ", firstName='" + firstName + '\'' +
+                    ", LastName='" + LastName + '\'' +
+                    '}';
+        }
     }
 }
