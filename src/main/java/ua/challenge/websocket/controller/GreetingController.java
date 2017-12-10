9da73a70f0157d09668e4a9191f19a2e6d713b95
Bakal <d.bakal@controlpay.com>
2017-12-10 10:56:05 +0200

    Spring: WebSocket - using @DestinationVariable

--- src/main/java/ua/challenge/websocket/controller/GreetingController.java
+++ src/main/java/ua/challenge/websocket/controller/GreetingController.java
@@ -2,6 +2,7 @@ package ua.challenge.websocket.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -23,10 +24,10 @@ public class GreetingController {
         return new Greeting("Hello, " + message.getName() + "!");
     }
 
-    @MessageMapping("/actions")
-    public void actions(ActionMessage message) throws InterruptedException {
-        log.info("Handling action with id: " + message.getId());
+    @MessageMapping("/actions/{id}")
+    public void actions(@DestinationVariable("id") Long id, ActionMessage message) throws InterruptedException {
+        log.info("Handling action with id: " + id);
         Thread.sleep(3000L);
-        this.template.convertAndSend("/topic/actions/history/" + message.getId(), "Action: " + message.getValue());
+        this.template.convertAndSend("/topic/actions/history/" + id, "Action: " + message.getValue());
     }
 }
